9c63d95ef326704afa0f8e55917b72576db159c1
RaminRzayev <raminag@code.edu.az>
2022-03-01 08:42:20 +0400

    Add Task

---
+++ P127.HW.28.02/P127.HW.28.02/Extension.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P127.HW._28._02
+{
+   static class Extension
+    {
+
+
+        public static string ToCapitalize(this string str)
+        {
+            string result="";
+            string[] arr = str.Split(" ");
+            foreach (var item in arr)
+            {
+                item.ToLower();
+
+                Char.ToUpper(item[0]);
+                result =String.Concat(arr);
+
+               
+            }
+
+            return result;
+        }
+
+
+
+
+        public static bool ToSearch(this string str, string srch)
+        {
+
+            for (int i = 0; i < str.Length; i++)
+            {
+
+                for (int j = 0; j < srch.Length; j++)
+                {
+                    if (str[i]==srch[j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+
+        }
+    }
+}
---
+++ P127.HW.28.02/P127.HW.28.02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace P127.HW._28._02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "daDA dadwA dasdWWW";
+            Console.WriteLine(str.ToCapitalize());
+
+            /*string str = "Admin";
+            Console.WriteLine(str.ToSearch("A"));*/
+
+        }
+    }
+}
